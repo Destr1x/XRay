0be262eb9d571e4bda22c880b0a1a28400bc92b2
Destr1x <ManzhosArtem823@gmail.com>
2024-09-26 15:52:18 +0200

    Xray by Destr1x

---
+++ Xray.js
@@ -0,0 +1,74 @@
+import { world, system } from "@minecraft/server";
+
+const ores = [
+    "minecraft:coal_ore",
+    "minecraft:iron_ore",
+    "minecraft:gold_ore",
+    "minecraft:diamond_ore",
+    "minecraft:emerald_ore",
+    "minecraft:lapis_ore",
+    "minecraft:redstone_ore",
+];
+
+let state = {};
+
+function hideBlocks(player) {
+	const dimension = player.dimension;
+	const radius = 1000;
+	const pos = player.location;
+	
+	for (let x = -radius; x <= radius; x++) {
+        for (let y = -radius; y <= radius; y++) {
+            for (let z = -radius; z <= radius; z++) {
+                const blockPos = {
+                    x: Math.floor(playerPos.x) + x,
+                    y: Math.floor(playerPos.y) + y,
+                    z: Math.floor(playerPos.z) + z,
+                };
+
+                const block = dimension.getBlock(blockPos);
+                if (!ores.includes(block.typeId)) {
+                    player.runCommandAsync(`все блоки стали невидемыми кроме руд`);
+                }
+            }
+        }
+    }
+}
+
+function restoreBlocks(player) {
+	const dimension = player.dimension;
+    const radius = 10;
+    const playerPos = player.location;
+
+    for (let x = -radius; x <= radius; x++) {
+        for (let y = -radius; y <= radius; y++) {
+            for (let z = -radius; z <= radius; z++) {
+                const blockPos = {
+                    x: Math.floor(playerPos.x) + x,
+                    y: Math.floor(playerPos.y) + y,
+                    z: Math.floor(playerPos.z) + z,
+                };
+
+                const block = dimension.getBlock(blockPos);
+
+                player.runCommandAsync(`Все блоки стали видемыми`);
+            }
+        }
+    }
+}
+
+system.events.beforeItemUse.subscribe(event => {
+    const player = event.source;
+
+    if (event.item.id === "minecraft:grass") {
+        if (!state[player.name]) {
+            state[player.name] = true;
+            hideBlocks(player);
+            player.sendMessage("Все блоки стали невидемыми кроме руды");
+        } else {
+            state[player.name] = false;
+            restoreBlocks(player);
+            player.sendMessage("Все блоки стали видемыми");
+        }
+    }
+});
